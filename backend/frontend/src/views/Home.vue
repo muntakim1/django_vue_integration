8acead9217b27325cf112c81c3b7332075dba0a7
Muntakimur Rahaman <32785930+muntakim1@users.noreply.github.com>
2019-03-03 15:52:43 +0600

    django with vuejs initial commit

---
+++ backend/frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+ <div>
+    <router-link to='/'>Home</router-link> |
+    <router-link to='/about'>About</router-link> |
+  <HelloWorld />
+ </div>
+
+</template>
+
+<script>
+  import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    components: {
+      HelloWorld
+    }
+  }
+</script>
